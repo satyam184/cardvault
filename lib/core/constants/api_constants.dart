8c47f2d31ebb5f2f3848ebdebaeb1adaa1966870
satyam184 <satyam379prasad@gmail.com>
2026-05-18 23:02:22 +0530

    fix: app constant

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static String get openRouterApiKey =>
-      dotenv.env['OPENROUTER_API_KEY'] ?? dotenv.env['GROQ_API_KEY'] ?? '';
+  static String get openRouterApiKey => dotenv.env['OPENROUTER_API_KEY'] ?? '';
   static String get baseUrl => 'https://cardvault-backend.luminoai.online';
 }
