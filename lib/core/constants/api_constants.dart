669c613fa048c0dda87186e571463f86203121b4
satyam184 <satyam379prasad@gmail.com>
2026-05-19 14:58:17 +0530

    feat: protected the base url

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,5 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static String get openRouterApiKey => dotenv.env['OPENROUTER_API_KEY'] ?? '';
-  static String get baseUrl => 'https://cardvault-backend.luminoai.online';
+  static String get baseUrl =>
+      dotenv.env['BASE_URL'] ?? 'https://cardvault-backend.luminoai.online';
 }
